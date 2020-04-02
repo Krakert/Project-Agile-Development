2a8114be3cc65e33fcdd230c1fb25817cf555166
stefan de kraker <sdekraker1999@live.nl>
2020-04-02 23:09:51 +0200

    Setup for sprint review 2

--- main.py
+++ main.py
@@ -36,8 +36,9 @@ def updateToZeo():
 def main():
     global dataFromMqtt
     client.loop_start()
+    client.publish(topic, "")
     print ("Connected to Mqtt")
-    nao.talk.say("Welokm bij de oplevering", openSession)
+    nao.audio.say("Welokm bij de oplevering", openSession)
     #NAO.runBuildInAnimation(openSession)       # Werkt door de Corona dus niet
 
     nao.head.moveHead(openSession, ["HeadYaw", "HeadPitch"], [0,0], [2,2])
@@ -51,10 +52,10 @@ def main():
             nao.head.moveHead(openSession, ["HeadYaw", "HeadPitch"], [0,0], [0.2,0.2])
             updateToZeo()
         elif dataFromMqtt == "3":
-            nao.arm.moveArm(openSession, "left", 0.5, ["ShoulderRoll", "ElbowYaw"], [90, 30], 0.3)
+            nao.arm.moveArm(openSession, "left", 0.5, ["ShoulderRoll", "ElbowYaw"], [70, 30], 0.3)
             updateToZeo()
         elif dataFromMqtt == "4":
-            nao.arm.moveArm(openSession, "right", 0.5, ["ShoulderRoll", "ElbowYaw"], [90, 30], [2, 0.3])
+            nao.arm.moveArm(openSession, "right", 0.5, ["ShoulderRoll", "ElbowYaw"], [70, 30], [2, 0.3])
             updateToZeo()
         elif dataFromMqtt == "5":
             nao.arm.moveArmToRestPosistion(openSession, "left")
@@ -62,6 +63,15 @@ def main():
         elif dataFromMqtt == "6":
             nao.arm.moveArmToRestPosistion(openSession, "right")
             updateToZeo()
-
+        elif dataFromMqtt == "7":
+            nao.arm.moveArm(openSession, "left", 1, ["ShoulderPitch", "ShoulderRoll", "ElbowYaw", "Hand"], [-71, 17, -5, 1.0], 1)
+            nao.arm.moveArm(openSession, "left", 1, ["ElbowRoll"], [-65], 0.3)
+            nao.arm.moveArm(openSession, "left", 1, ["ElbowRoll"], [-2], 0.3)
+            nao.arm.moveArm(openSession, "left", 1, ["ElbowRoll"], [-65], 0.3)
+            nao.arm.moveArm(openSession, "left", 1, ["ElbowRoll"], [-2], 0.3)
+            dataFromMqtt = "5"
+        elif dataFromMqtt == "8":
+            nao.leg.talkAndWalk(openSession, "Wat leuk zo`n oplevering digitaal", 2, [0.2, 0])
+            updateToZeo()
 if __name__ == '__main__':
     main()
