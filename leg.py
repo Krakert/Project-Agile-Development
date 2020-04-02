a70bc125c513139b492a8315173f4743b3e46dc8
stefan de kraker <sdekraker1999@live.nl>
2020-04-02 23:09:04 +0200

    Fucntion to walk x meters

--- leg.py
+++ leg.py
@@ -1,22 +1,12 @@
 import time
-from naoqi import ALProxy
 class leg:
-    name = None
+    def __init__(self):
+        pass
 
-    def __init__(self, name):
-        self.name = name
-
-    def move(self, session):
-        print("leg move")
-
+    def talkAndWalk(self, session, text, timeToSpeak, moveAmount = []):
         motion = session.service("ALMotion")
+        tts    = session.service("ALTextToSpeech")
         motion.moveInit()
-        motion.moveTo(0.1, 0.1, 0)
-
-    def talkAndWalk(self, text, session):
-        motion = ALProxy("ALMotion", "padrick.local", 9559)
-        tts    = ALProxy("ALTextToSpeech", "padrick.local", 9559)
-        motion.moveInit()
-        motion.post.moveTo(0.3, 0, 0)
-        time.sleep(1)
+        motion.moveTo(moveAmount[0], moveAmount[1], 0)
+        time.sleep(timeToSpeak)
         tts.say(text)
