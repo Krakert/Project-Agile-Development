9d83c6f9cd97d6a2964b4ebc542e5152e107f2ec
stefan de kraker <sdekraker1999@live.nl>
2020-04-02 23:08:10 +0200

    Function made to move arm to a point

--- arm.py
+++ arm.py
@@ -1,29 +1,33 @@
 import math
 class arm:
+    def __init__(self):
+        pass
 
-    def moveArm(self, session, left_Right, stiffness, joints = [], angles = [], timeTocomplete = None):
+    def moveArm(self, session, leftRight, stiffness, joints = [], angles = [], timeTocomplete = None):
         correctJoints = []
-        correctAngles = []
+        arrayOfRadInvert = []
         arrayOfRad = []
         arrayOfTimeToComplete = []
         angleLists = None
         for x in range(len(angles)):                                                                    # Convert all angels to Rad
-            arrayOfRad.append(math.radians(angles[x]))                                                  # And add to new array
-
+            if joints[x] == "Hand":
+                arrayOfRad.append(angles[x])
+            else:
+                arrayOfRad.append(math.radians(angles[x]))                                              # And add to new array
         motion = session.service("ALMotion")                                                            # use the AlMotion API
 
-        if left_Right.lower() == "left":                                                                # Check wich side to use
+        if leftRight.lower() == "left":                                                                 # Check wich side to use
             for x in range(len(joints)):
                 correctJoints.append("L" + joints[x])
             motion.setStiffnesses("LArm", stiffness)
             angleLists = arrayOfRad
 
-        elif left_Right.lower() == "right":
+        elif leftRight.lower() == "right":
             for x in range(len(joints)):
                 correctJoints.append("R" + joints[x])
-                correctAngles.append(arrayOfRad[x] * - 1)                                               # Right needs negative angels, invert them.
-            left_Right
-            angleLists = correctAngles
+                arrayOfRadInvert.append(arrayOfRad[x] * - 1)                                            # Right needs negative angels, invert them.
+            leftRight
+            angleLists = arrayOfRadInvert
 
         if type(timeTocomplete) is float:                                                               # Overloading, one speed for al joints
             for j in range (len(joints)):                                                               # Or all joints there own speed
@@ -34,22 +38,18 @@ class arm:
 
         isAbsolute = True                                                                               # Absolute move, False for relative
         motion.angleInterpolation(correctJoints, angleLists, timeLists, isAbsolute)                     # Execute movement
-        arrayOfRad = []                                                                                 # Clear al the old data
-        correctAngles = []
-        correctJoints = []
-        arrayOfTimeToComplete = []
 
-    def moveArmToRestPosistion(self, session, left_Right):
 
+    def moveArmToRestPosistion(self, session, leftRight):                                               # Move the arms to start posistion
         motion = session.service("ALMotion")                                                            # use the AlMotion API
-        if left_Right.lower() == "left":
+        if leftRight.lower() == "left":
             motion.setStiffnesses("LArm", 0.5)
             motion.angleInterpolation(["LShoulderPitch", "LShoulderRoll",
                                        "LElbowYaw", "LElbowRoll",
                                        "LWristYaw", "LHand"],
                                       [1.43, 0.2, -1.18,
                                        -0.41, 0.10, 0.3], [2, 2, 2, 2, 2, 2], True)
-        elif left_Right.lower() == "right":
+        elif leftRight.lower() == "right":
             motion.setStiffnesses("RArm", 0.5)
             motion.angleInterpolation(["RShoulderPitch", "RShoulderRoll",
                                        "RElbowYaw", "RElbowRoll",
