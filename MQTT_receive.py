770954ebcc9453052b447a40257fb77e5ec0adde
stefan de kraker <sdekraker1999@live.nl>
2020-04-02 10:56:27 +0200

    Working in progress

--- MQTT_receive.py
+++ MQTT_receive.py
@@ -1,3 +1,5 @@
+# Simple test code for MQTT, when a message is send, the message will be printed.
+
 import paho.mqtt.client as mqtt
 
 def on_message(client, userdata, msg):
@@ -5,12 +7,10 @@ def on_message(client, userdata, msg):
 
 
 broker = "mqtt.hva-robots.nl"
-client = mqtt.Client("krakers_2")
+client = mqtt.Client("PC_receiver")
 client.username_pw_set(username="krakers", password="kuNH5LNWptsGrPfL6Azh")
-
-client.connect(broker)  # connect to broker
+client.connect(broker)
 client.subscribe("krakers/PAD")
-
 client.loop_start()
 
 while True:
