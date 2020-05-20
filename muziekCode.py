d937799f00210ff5583c4840bc185f1fc918d492
Renze Goossens <renze.goossens@hva.nl>
2020-05-20 14:11:09 +0200

    code meer reusable gemaakt

--- muziekCode.py
+++ muziekCode.py
@@ -43,35 +43,49 @@ def choose_song(choice):
 
     # animation_player.run("")
     # animation_player.runTag("hello")
-
+    #defineer animaties
     wave = "animations/Stand/Gestures/Hey_1"
+
+    #defineer locatie liedjes
+    song_1 = "/home/nao/wav/goosser4_1587549525.mp3"
+    song_2 = "/home/nao/wav/goosser4_1587549611.mp3"
+    song_3 = "/home/nao/wav/goosser4_1587549697.mp3"
+    song_4 = "/home/nao/wav/goosser4_1587549774.mp3"
+    song_5 = "/home/nao/wav/goosser4_1587549697.mp3"
+
+    song_name1 = "Aan de amsterdamse grachten van Wim Sonneveld"
+    song_name2 = "Geef mij maar Amsterdam gekozen van Johnny Jordaan"
+    song_name3 = "Opzij opzij opzij van Herman van Veen"
+    song_name4 = "Ik ben vandaag zo vrolijk van Herman van Veen"
+    song_name5 = "twee motten van Dorus"
+
     posture.goToPosture("StandInit", 1.0)
     time.sleep(2.0)
 
     if choice is 1:
-        tts.say("U heeft het nummer Aan de amsterdamse grachten gekozen van Wim Sonneveld.")
+        tts.say("U heeft het nummer" + song_name1 + " gekozen.")
         qi.async(animation_player.run, wave)
-        qi.async(audio_Player.playFile, "/home/nao/wav/goosser4_1587549525.mp3")
+        qi.async(audio_Player.playFile, song_1)
         tts.say("Wat een klassieker!")
     elif choice is 2:
-        tts.say("U heeft het nummer Aan Geef mij maar Amsterdam gekozen van Johnny Jordaan.")
+        tts.say("U heeft het nummer" + song_name2 + " gekozen.")
         qi.async(animation_player.run, wave)
-        qi.async(audio_Player.playFile, "/home/nao/wav/goosser4_1587549611.mp3")
+        qi.async(audio_Player.playFile, song_2)
         tts.say("Geef mij maar Amsterdam, want in Rotjeknor heb ik niks te zoeken.")
     elif choice is 3:
-        tts.say("U heeft het nummer Aan Opzij opzij opzij van Herman van Veen.")
+        tts.say("U heeft het nummer" + song_name3 + " gekozen.")
         qi.async(animation_player.run, wave)
-        qi.async(audio_Player.playFile, "/home/nao/wav/goosser4_1587549697.mp3")
+        qi.async(audio_Player.playFile, song_3)
         tts.say("Nou voor uit dan maar. Ik ga wel een stapje opzij.")
     elif choice is 4:
-        tts.say("U heeft het nummer Aan Ik ben vandaag zo vrolijk van Herman van Veen.")
+        tts.say("U heeft het nummer" + song_name4 + " gekozen.")
         qi.async(animation_player.run, wave)
-        qi.async(audio_Player.playFile, "/home/nao/wav/goosser4_1587549774.mp3")
+        qi.async(audio_Player.playFile, song_4)
         tts.say("Ik ben van dit nummer een stukje vrolijker geworden. U hopelijk ook!")
     elif choice is 5:
-        tts.say("U heeft het nummer Aan twee motten van Dorus.")
+        tts.say("U heeft het nummer" + song_name5 + " gekozen.")
         qi.async(animation_player.run, wave)
-        qi.async(audio_Player.playFile, "/home/nao/wav/goosser4_1587549697.mp3")
+        qi.async(audio_Player.playFile, song_5)
         tts.say("aaah altijd leuk zo een liedje van vroeger.")
     else:
         tts.say("Sorry, deze optie bestaat niet.")
@@ -80,7 +94,7 @@ def main():
     # open sessie
     session = qi.Session()
     session.connect("padrick.robot.hva-robots.nl:9559")
-    
+
     choose_song(6)
 
 
