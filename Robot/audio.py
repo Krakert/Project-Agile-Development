ccd9ca34caa875507fa3dc920967e1ac69d802d1
S. de Kraker <stefan.de.kraker@hva.nl>
2020-05-27 17:08:57 +0200

    Playback autofiles

--- Robot/audio.py
+++ Robot/audio.py
@@ -1,11 +1,24 @@
+from naoqi import qi
+
+audio = None
 class audio:
 
     def say(self, text,session):
         textToSpeechService = session.service("ALTextToSpeech")
-        print (text)
         textToSpeechService.say(text)
 
-    def playAudioFile(self,session, talkBefore, talkAfter):
-        self.say(talkBefore, session)
+
+    def playAudioFile(self,session, talkBefore, animationToPlay, audioFileToPlay):
+        global audio
+        if talkBefore is not None:
+            self.say(talkBefore, session)
         animation_player = session.service("ALAnimationPlayer")
         audioPlayer = session.service("ALAudioPlayer")
+        qi.async(animation_player.run, animationToPlay)
+        qi.async(audioPlayer.playFile, audioFileToPlay)
+
+    def killAudio(self, session):
+        speechPlayer = session.service("ALTextToSpeech")
+        audioPlayer = session.service("ALAudioPlayer")
+        audioPlayer.stopAll()
+        speechPlayer.stopAll()
